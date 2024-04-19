08a38b62551164ee6d46a92f2502d829e15e581f
Dusk <asutoshpradhan424@gmail.com>
2024-04-20 05:26:50 +0530

    Add final Touches in styles.cc:     - add header and flex elements     - add fonts Add Font files into fonts folder:     - coolvetical regular     - griffy regular

--- sketch.js
+++ sketch.js
@@ -15,13 +15,13 @@ function createGrid(size){
     while(container.firstChild){
         container.removeChild(container.firstChild);
     }
-    container.setAttribute("style","display: flex; gap: 1px;flex: 1; flex-direction: column; width: 800px; height: 800px; margin-top: 30px")
+    container.setAttribute("style","background-color: white; padding: 5px;border: dashed black 5px;display: flex; gap: 0px;flex: 1; flex-direction: column; width: 800px; height: 800px; margin-top: 30px")
     for(i = 0; i<size;i++){
         const row = document.createElement("div");
-        row.setAttribute("style", `display: flex; flex: 1; gap: 1px; height: ${960/size}`);
+        row.setAttribute("style", `display: flex; flex: 1; gap: 0px; height: ${960/size}`);
         for(j = 0; j<size;j++){
             const cell = document.createElement("div");
-            cell.setAttribute("style", "border: solid black 1px; flex: 1;");
+            cell.setAttribute("style", " flex: 1;");
             row.appendChild(cell);
             cell.classList.add("cell");
         } 
@@ -29,7 +29,7 @@ function createGrid(size){
     }
     const cells = document.querySelectorAll(".cell");
     cells.forEach(cell => {
-        cell.addEventListener("mouseout", () => {
+        cell.addEventListener("mouseover", () => {
             cell.style.backgroundColor = `rgb(${Math.random()*256},${Math.random()*256},${Math.random()*256})`;
         });
     });
