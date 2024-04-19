dab522afb2833184b509d0a1cd4d4b9697844869
Dusk <asutoshpradhan424@gmail.com>
2024-04-20 04:52:13 +0530

    Add functionality through javascript and css:     - add hovering effect to the divs     - add changing color attribute on hovering

--- sketch.js
+++ sketch.js
@@ -15,7 +15,7 @@ function createGrid(size){
     while(container.firstChild){
         container.removeChild(container.firstChild);
     }
-    container.setAttribute("style","display: flex; gap: 1px;flex: 1; flex-direction: column; width: 960px; height: 960px; margin-top: 30px")
+    container.setAttribute("style","display: flex; gap: 1px;flex: 1; flex-direction: column; width: 800px; height: 800px; margin-top: 30px")
     for(i = 0; i<size;i++){
         const row = document.createElement("div");
         row.setAttribute("style", `display: flex; flex: 1; gap: 1px; height: ${960/size}`);
@@ -24,11 +24,15 @@ function createGrid(size){
             cell.setAttribute("style", "border: solid black 1px; flex: 1;");
             row.appendChild(cell);
             cell.classList.add("cell");
-        }
-        
+        } 
         container.appendChild(row);
     }
+    const cells = document.querySelectorAll(".cell");
+    cells.forEach(cell => {
+        cell.addEventListener("mouseout", () => {
+            cell.style.backgroundColor = `rgb(${Math.random()*256},${Math.random()*256},${Math.random()*256})`;
+        });
+    });
 }
 createGrid(16);
 
-document.styleSheets[0].insertRule(".cell:hover {background-color: black}")
