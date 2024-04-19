8aca7290e4595ccad56a713a494fcf5ba0152105
Dusk <asutoshpradhan424@gmail.com>
2024-04-20 04:29:59 +0530

    Create basic html structure:     - add grid size button     - add container Make changes to grid size from javascript DOM

---
+++ sketch.js
@@ -0,0 +1,34 @@
+function changeGrid(){
+    let size;
+    while(true){
+        size=prompt("Enter the size of grid (16 - 100): ");
+        if(size<16 || size>100)
+            continue;
+        else
+            break;
+    }
+    createGrid(size);
+}
+
+function createGrid(size){
+    const container = document.querySelector(".container");
+    while(container.firstChild){
+        container.removeChild(container.firstChild);
+    }
+    container.setAttribute("style","display: flex; gap: 1px;flex: 1; flex-direction: column; width: 960px; height: 960px; margin-top: 30px")
+    for(i = 0; i<size;i++){
+        const row = document.createElement("div");
+        row.setAttribute("style", `display: flex; flex: 1; gap: 1px; height: ${960/size}`);
+        for(j = 0; j<size;j++){
+            const cell = document.createElement("div");
+            cell.setAttribute("style", "border: solid black 1px; flex: 1;");
+            row.appendChild(cell);
+            cell.classList.add("cell");
+        }
+        
+        container.appendChild(row);
+    }
+}
+createGrid(16);
+
+document.styleSheets[0].insertRule(".cell:hover {background-color: black}")
